894bf53fc1268166d03693bd8704a2d1c8ec908d
Md. Jubayer Shikder <validjubayer@gmail.com>
2025-11-27 12:31:37 +0600

    feat: add new post method with on console buffer and returning the same data its been given

--- nodejs/creating a server/src/server.ts
+++ nodejs/creating a server/src/server.ts
@@ -1,21 +1,61 @@
-import { config } from "dotenv";
 import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log("server is running");
 
-    res.writeHead(200, { "content-type": "application/json" });
-    res.end(
-      JSON.stringify({
-        message: "Hello from nodejs with typescript",
-        path: req.url,
-      })
-    );
+    //root route
+    if (req.url === "/" && req.method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Hello from nodejs with typescript",
+          path: req.url,
+        })
+      );
+    }
+
+    // /api route
+    if (req.url === "/api" && req.method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Hello from /api",
+          path: req.url,
+        })
+      );
+    }
+
+    // post route
+    if (req.url === "/api/users" && req.method === "POST") {
+      // console.log(req.method, req.url);
+      // res.writeHead(200, { "content-type": "application/json" });
+      const user = {
+        id: 1,
+        name: "alice",
+      };
+      let body = "";
+      // listen data chunk
+      req.on("data", (chunk) => {
+        console.log(chunk);
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+       try {
+         const parsedBody = JSON.parse(body);
+        console.log(parsedBody);
+        res.end(JSON.stringify(parsedBody));
+       } catch (error: any) {
+        console.log(error.message);
+       }
+      });
+    }
+
   }
 );
 
-
-server.listen(5000, () => {
-    console.log("server is running from port 5000")
-})
+server.listen(config.port, () => {
+  console.log("server is running from port 5000");
+});
