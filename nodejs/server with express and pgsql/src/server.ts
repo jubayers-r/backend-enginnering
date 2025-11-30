fb491b995d61cd5349ae709ff1f791b45a13eba0
Jubayer <validjubayer@gmail.com>
2025-11-30 15:41:36 +0600

    feat: add CRUD for todos (depends on user_id)

--- nodejs/server with express and pgsql/src/server.ts
+++ nodejs/server with express and pgsql/src/server.ts
@@ -56,6 +56,33 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello world damn son");
 });
 
+// usersDB starts
+
+// post requests
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      `
+    INSERT INTO users(name, email) VALUES($1, $2) RETURNING *
+    `,
+      [name, email]
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Data inserted",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
 // get requests
 app.get("/users", async (req: Request, res: Response) => {
   try {
@@ -63,11 +90,18 @@ app.get("/users", async (req: Request, res: Response) => {
       SELECT * FROM users
       `);
 
-    res.status(200).json({
-      success: true,
-      message: "Users retrived successfully",
-      data: result.rows,
-    });
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "Not found any data on users table",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Users retrived successfully",
+        data: result.rows,
+      });
+    }
   } catch (error: any) {
     res.status(500).json({
       success: false,
@@ -105,31 +139,6 @@ app.get("/users/:id", async (req: Request, res: Response) => {
       success: false,
       message: error.message,
       details: error,
-    });
-  }
-});
-
-// post requests
-app.post("/users", async (req: Request, res: Response) => {
-  const { name, email } = req.body;
-
-  try {
-    const result = await pool.query(
-      `
-    INSERT INTO users(name, email) VALUES($1, $2) RETURNING *
-    `,
-      [name, email]
-    );
-
-    res.status(201).json({
-      success: true,
-      message: "Data inserted",
-      data: result.rows[0],
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
     });
   }
 });
@@ -199,3 +208,123 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
     });
   }
 });
+
+// usersDB ends
+
+// todosDB starts
+
+app.post("/todos/:user_id", async (req: Request, res: Response) => {
+  const userId = req.params.user_id;
+  const { title } = req.body;
+
+  try {
+    const result = await pool.query(
+      `
+      INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *
+      `,
+      [userId, title]
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "todo created",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+});
+
+app.get("/todos/:user_id", async (req: Request, res: Response) => {
+  const userId = req.params.user_id;
+
+  try {
+    const result = await pool.query(
+      `
+      SELECT * FROM todos WHERE user_id=$1
+      `,
+      [userId]
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "todos retrived succesfully",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+});
+
+app.put("/todos/:user_id/:id", async (req: Request, res: Response) => {
+  const { user_id, id } = req.params;
+  const { title } = req.body;
+
+  try {
+    const result = await pool.query(
+      `
+      UPDATE todos SET title=$1 WHERE user_id=$2 AND id=$3 RETURNING *
+      `,
+      [title, user_id, id]
+    );
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "todo not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "succesfully updated",
+        data: result.rows[0],
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      details: error,
+    });
+  }
+});
+
+app.delete("/todos/:user_id/:id", async (req: Request, res: Response) => {
+  const { user_id, id } = req.params;
+
+  try {
+    const result = await pool.query(
+      `
+      DELETE FROM todos WHERE user_id=$1 AND id=$2
+      `,
+      [user_id, id]
+    );
+
+    if (result.rowCount! > 0) {
+      res.status(200).json({
+        success: true,
+        message: "todos deleted succesfully",
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "todo not found",
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+});
+
+// todosDB ends
