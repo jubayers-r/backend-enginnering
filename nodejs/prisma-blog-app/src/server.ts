3638010028aa822a016a510ac30e6971830ddad5
Md. Jubayer Shikder <validjubayer@gmail.com>
2025-12-27 23:13:32 +0600

    feat: init server with db

---
+++ nodejs/prisma-blog-app/src/server.ts
@@ -0,0 +1,21 @@
+import app from "./app";
+import { prisma } from "./lib/prisma";
+
+const PORT = process.env.PORT || 5000;
+
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the db successfully");
+
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("An error occured:", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
